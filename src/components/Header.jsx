9a4df06ba254195dd3ce3e3b6a486e3459fa5474
LeHuuKienTinh <lehkientinh@dtu.edu.vn>
2025-08-25 21:03:16 +0700

    2025-08-25-Init-Project-Portfolite

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>Header
+      <nav>
+        <ul>
+          <li><a href="projects">Project</a></li>
+          <li><a href="skills">Skills</a></li>
+          <li><a href="contact">Contact</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="blog">Blog</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
